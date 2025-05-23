102f21a88a3bc4def578674aecc5c94bf2291bf2
Luca Pasca <koreluca@proton.me>
2025-05-23 07:33:20 +0300

    Updated shopping cart hover animation and copyright year to 2025

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ app.post('/create-payment-intent', async (req, res) => {
                 </div>
 
                 <div style="text-align: center; margin-top: 30px; padding-top: 20px; border-top: 1px solid #eee;">
-                    <p style="color: #999; font-size: 12px;">© 2024 Beatlist. All rights reserved.</p>
+                    <p style="color: #999; font-size: 12px;">© 2025 Beatlist. All rights reserved.</p>
                 </div>
             </div>
         `;
